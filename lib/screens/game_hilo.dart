e37eb5a6b9450754a9187d607e51794660377b2b
Kaj Rusilowski <xedom08@gmail.com>
2024-06-16 01:34:02 +0200

    update

--- lib/screens/game_hilo.dart
+++ lib/screens/game_hilo.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class GameHiLoPage extends StatefulWidget {
@@ -62,13 +61,17 @@ class _GameHiLoPageState extends State<GameHiLoPage> {
     'spades_10',
     'spades_11',
     'spades_12',
-    'spades_13'
+    'spades_13',
+    'joker'
   ];
 
   String _currentCard = '';
   String _nextCard = '';
   String _message = '';
   int _score = 0;
+  List<String> _previousCards = [];
+  final TextEditingController _betController = TextEditingController();
+  int _coins = 100;
 
   @override
   void initState() {
@@ -81,8 +84,45 @@ class _GameHiLoPageState extends State<GameHiLoPage> {
     return _deck[random.nextInt(_deck.length)];
   }
 
+  double _calculateProbability(bool isHigher) {
+    int currentCardValue = _getCardValue(_currentCard);
+    int higherCount =
+        _deck.where((card) => _getCardValue(card) > currentCardValue).length;
+    int lowerCount =
+        _deck.where((card) => _getCardValue(card) < currentCardValue).length;
+
+    return isHigher
+        ? (higherCount / _deck.length) * 100
+        : (lowerCount / _deck.length) * 100;
+  }
+
+  double _calculateJokerProbability() {
+    return (1 / _deck.length) * 100;
+  }
+
+  double _calculateNumberProbability() {
+    int numberCount = _deck.where((card) => _isNumber(card)).length;
+    return (numberCount / _deck.length) * 100;
+  }
+
+  double _calculateFigureProbability() {
+    int figureCount = _deck.where((card) => _isFigure(card)).length;
+    return (figureCount / _deck.length) * 100;
+  }
+
+  double _calculateColorProbability(bool isRed) {
+    int colorCount = _deck.where((card) => _isRed(card) == isRed).length;
+    return (colorCount / _deck.length) * 100;
+  }
+
   void _guess(bool isHigher) {
     setState(() {
+      int bet = int.tryParse(_betController.text) ?? 0;
+      if (bet > _coins || bet <= 0) {
+        _message = 'Invalid bet amount';
+        return;
+      }
+      _coins -= bet;
       _nextCard = _getRandomCard();
       int currentCardValue = _getCardValue(_currentCard);
       int nextCardValue = _getCardValue(_nextCard);
@@ -90,16 +130,120 @@ class _GameHiLoPageState extends State<GameHiLoPage> {
       if ((isHigher && nextCardValue > currentCardValue) ||
           (!isHigher && nextCardValue < currentCardValue)) {
         _score++;
-        _message = 'Correct!';
+        _coins += bet * 2;
+        _message = 'Correct! You won $bet coins.';
       } else {
         _score = 0;
-        _message = 'Wrong!';
+        _message = 'Wrong! You lost $bet coins.';
+      }
+      _previousCards.add(_currentCard);
+      if (_previousCards.length > 10) {
+        _previousCards.removeAt(0);
       }
       _currentCard = _nextCard;
     });
   }
 
+  void _guessJoker() {
+    setState(() {
+      int bet = int.tryParse(_betController.text) ?? 0;
+      if (bet > _coins || bet <= 0) {
+        _message = 'Invalid bet amount';
+        return;
+      }
+      _coins -= bet;
+      _nextCard = _getRandomCard();
+
+      if (_nextCard == 'joker') {
+        _score += 5; // bonus for guessing joker
+        _coins += bet * 10;
+        _message = 'Correct! It\'s a Joker! You won $bet coins.';
+      } else {
+        _score = 0;
+        _message = 'Wrong! It\'s not a Joker. You lost $bet coins.';
+      }
+      _previousCards.add(_currentCard);
+      if (_previousCards.length > 10) {
+        _previousCards.removeAt(0);
+      }
+      _currentCard = _nextCard;
+    });
+  }
+
+  void _guessNumberOrFigure(bool isNumber) {
+    setState(() {
+      int bet = int.tryParse(_betController.text) ?? 0;
+      if (bet > _coins || bet <= 0) {
+        _message = 'Invalid bet amount';
+        return;
+      }
+      _coins -= bet;
+      _nextCard = _getRandomCard();
+      bool isNextCardNumber = _isNumber(_nextCard);
+
+      if ((isNumber && isNextCardNumber) || (!isNumber && !isNextCardNumber)) {
+        _score++;
+        _coins += bet * 2;
+        _message = 'Correct! You won $bet coins.';
+      } else {
+        _score = 0;
+        _message = 'Wrong! You lost $bet coins.';
+      }
+      _previousCards.add(_currentCard);
+      if (_previousCards.length > 10) {
+        _previousCards.removeAt(0);
+      }
+      _currentCard = _nextCard;
+    });
+  }
+
+  void _guessColor(bool isRed) {
+    setState(() {
+      int bet = int.tryParse(_betController.text) ?? 0;
+      if (bet > _coins || bet <= 0) {
+        _message = 'Invalid bet amount';
+        return;
+      }
+      _coins -= bet;
+      _nextCard = _getRandomCard();
+
+      if (_isRed(_nextCard) == isRed) {
+        _score++;
+        _coins += bet * 2;
+        _message = 'Correct! You won $bet coins.';
+      } else {
+        _score = 0;
+        _message = 'Wrong! You lost $bet coins.';
+      }
+      _previousCards.add(_currentCard);
+      if (_previousCards.length > 10) {
+        _previousCards.removeAt(0);
+      }
+      _currentCard = _nextCard;
+    });
+  }
+
+  bool _isNumber(String card) {
+    if (card == 'joker') return false;
+    int value = int.parse(card.split('_')[1]);
+    return value >= 2 && value <= 9;
+  }
+
+  bool _isFigure(String card) {
+    if (card == 'joker') return false;
+    int value = int.parse(card.split('_')[1]);
+    return value == 1 || value >= 11;
+  }
+
+  bool _isRed(String card) {
+    if (card.startsWith('hearts') || card.startsWith('diamonds')) {
+      return true;
+    }
+    return false;
+  }
+
   int _getCardValue(String card) {
+    if (card == 'joker') return 14; // Joker is considered the highest value
     return int.parse(card.split('_')[1]);
   }
 
@@ -107,30 +251,112 @@ class _GameHiLoPageState extends State<GameHiLoPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Current Card:', style: TextStyle(fontSize: 24)),
-            const SizedBox(height: 20),
-            Image.asset('assets/cards/$_currentCard.png', height: 150),
-            const SizedBox(height: 20),
-            Text(
-              _message,
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                    onPressed: () => _guess(true), child: const Text('Higher')),
-                const SizedBox(width: 20),
-                ElevatedButton(
-                    onPressed: () => _guess(false), child: const Text('Lower')),
-              ],
-            ),
-          ],
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Current Card:', style: TextStyle(fontSize: 24)),
+              const SizedBox(height: 10),
+              Image.asset('assets/cards/$_currentCard.png', height: 300),
+              const SizedBox(height: 10),
+              Text(
+                _message,
+                style:
+                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                controller: _betController,
+                decoration: const InputDecoration(
+                  labelText: 'Bet Amount',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => _guess(true),
+                    child: Text(
+                        'Higher (${_calculateProbability(true).toStringAsFixed(1)}%)'),
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () => _guess(false),
+                    child: Text(
+                        'Lower (${_calculateProbability(false).toStringAsFixed(1)}%)'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: _guessJoker,
+                child: Text(
+                    'Joker (${_calculateJokerProbability().toStringAsFixed(1)}%)'),
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => _guessNumberOrFigure(true),
+                    child: Text(
+                        'Number: 2-9 (${_calculateNumberProbability().toStringAsFixed(1)}%)'),
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () => _guessNumberOrFigure(false),
+                    child: Text(
+                        'Figure: JQKA (${_calculateFigureProbability().toStringAsFixed(1)}%)'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => _guessColor(true),
+                    child: Text(
+                        'Red (${_calculateColorProbability(true).toStringAsFixed(1)}%)'),
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () => _guessColor(false),
+                    child: Text(
+                        'Black (${_calculateColorProbability(false).toStringAsFixed(1)}%)'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              const Text('Previous Cards:', style: TextStyle(fontSize: 18)),
+              const SizedBox(height: 10),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: _previousCards.reversed
+                      .take(10)
+                      .map((card) => Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 2.0),
+                            child: Transform.translate(
+                              offset: const Offset(-10, 0),
+                              child: Image.asset('assets/cards/$card.png',
+                                  height: 80),
+                            ),
+                          ))
+                      .toList(),
+                ),
+              ),
+              const SizedBox(height: 10),
+              Text('Coins: $_coins',
+                  style: const TextStyle(
+                      fontSize: 18, fontWeight: FontWeight.bold)),
+            ],
+          ),
         ),
       ),
     );
