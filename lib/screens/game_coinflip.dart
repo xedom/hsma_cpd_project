194ad08822864b35f95db0389e8bb55afe806654
Kaj Rusilowski <xedom08@gmail.com>
2024-06-15 23:07:52 +0200

    new coinflip layout

--- lib/screens/game_coinflip.dart
+++ lib/screens/game_coinflip.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class GameCoinFlipPage extends StatefulWidget {
@@ -9,12 +8,37 @@ class GameCoinFlipPage extends StatefulWidget {
   _GameCoinFlipPageState createState() => _GameCoinFlipPageState();
 }
 
-class _GameCoinFlipPageState extends State<GameCoinFlipPage> {
+class _GameCoinFlipPageState extends State<GameCoinFlipPage>
+    with SingleTickerProviderStateMixin {
   final TextEditingController _betController = TextEditingController();
   String _userGuess = '';
   String _coinResult = '';
   String _message = '';
   bool _isHeads = true;
+  AnimationController? _animationController;
+  Animation<double>? _animation;
+  bool _showFront = true;
+  int _rotationCount = Random().nextInt(3) + 1;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    _animation = Tween<double>(begin: 0, end: 1).animate(_animationController!)
+      ..addListener(() {
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    _betController.dispose();
+    _animationController?.dispose();
+    super.dispose();
+  }
 
   void _flipCoin() {
     setState(() {
@@ -28,26 +52,67 @@ class _GameCoinFlipPageState extends State<GameCoinFlipPage> {
       } else {
         _message = 'You lost ${bet.toStringAsFixed(2)} coins!';
       }
+
+      _rotationCount = 1 + Random().nextInt(3);
+      _animationController?.duration =
+          Duration(seconds: 1 + Random().nextInt(3));
+
+      _animationController?.forward(from: 0).then((_) {
+        setState(() {
+          _showFront = !_showFront;
+        });
+      });
     });
   }
 
-  @override
-  void dispose() {
-    _betController.dispose();
-    super.dispose();
+  Widget _buildCoinImage() {
+    return AnimatedBuilder(
+      animation: _animation!,
+      builder: (context, child) {
+        final angle = _animation!.value * 2 * pi * _rotationCount;
+        final transform = Matrix4.identity()
+          ..setEntry(3, 2, 0.001)
+          ..rotateY(angle);
+        final showBack = (angle % (2 * pi)) > pi;
+
+        return Transform(
+          transform: transform,
+          alignment: Alignment.center,
+          child: Image.asset(
+            showBack
+                ? (_isHeads ? 'assets/tails.png' : 'assets/heads.png')
+                : (_isHeads ? 'assets/heads.png' : 'assets/tails.png'),
+            height: 300,
+            width: 300,
+          ),
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Coin Flip Game'),
-      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            // if (_coinResult.isNotEmpty)
+            Column(
+              children: [
+                _buildCoinImage(),
+                const SizedBox(height: 20),
+                Text(
+                  _message,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
             const Text(
               'Make a guess: Heads or Tails',
               style: TextStyle(fontSize: 24),
@@ -56,35 +121,45 @@ class _GameCoinFlipPageState extends State<GameCoinFlipPage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ElevatedButton(
-                  onPressed: () {
+                GestureDetector(
+                  onTap: () {
                     setState(() {
                       _userGuess = 'Heads';
                     });
                   },
-                  child: const Text('Heads'),
-                  // style: ElevatedButton.styleFrom(
-                  //   primary: _userGuess == 'Heads' ? Colors.blue : Colors.grey,
-                  // ),
+                  child: Image.asset(
+                    'assets/heads.png',
+                    height: 100,
+                    width: 100,
+                    color: _userGuess == 'Heads'
+                        ? Colors.blue.withOpacity(0.5)
+                        : null,
+                    colorBlendMode: BlendMode.color,
+                  ),
                 ),
                 const SizedBox(width: 20),
-                ElevatedButton(
-                  onPressed: () {
+                GestureDetector(
+                  onTap: () {
                     setState(() {
                       _userGuess = 'Tails';
                     });
                   },
-                  child: const Text('Tails'),
-                  // style: ElevatedButton.styleFrom(
-                  //   primary: _userGuess == 'Tails' ? Colors.blue : Colors.grey,
-                  // ),
+                  child: Image.asset(
+                    'assets/tails.png',
+                    height: 100,
+                    width: 100,
+                    color: _userGuess == 'Tails'
+                        ? Colors.blue.withOpacity(0.5)
+                        : null,
+                    colorBlendMode: BlendMode.color,
+                  ),
                 ),
               ],
             ),
             const SizedBox(height: 20),
             TextField(
               controller: _betController,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 labelText: 'Bet Amount',
                 border: OutlineInputBorder(),
               ),
@@ -94,28 +169,8 @@ class _GameCoinFlipPageState extends State<GameCoinFlipPage> {
             const SizedBox(height: 20),
             ElevatedButton(
               onPressed: _flipCoin,
-              child: Text('Flip Coin'),
+              child: const Text('Flip Coin'),
             ),
-            const SizedBox(height: 20),
-            if (_coinResult.isNotEmpty)
-              Column(
-                children: [
-                  Text(
-                    'Result: $_coinResult',
-                    style: TextStyle(fontSize: 24),
-                  ),
-                  SizedBox(height: 10),
-                  Image.asset(
-                    _isHeads ? 'assets/heads.png' : 'assets/tails.png',
-                    height: 100,
-                    width: 100,
-                  ),
-                  SizedBox(height: 20),
-                  Text(_message,
-                      style: const TextStyle(
-                          fontSize: 18, fontWeight: FontWeight.bold)),
-                ],
-              ),
           ],
         ),
       ),
