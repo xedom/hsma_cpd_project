3a819bf9825d2bf1a6861c0076583698bb17bd1c
MilanLukic01 <2122158@stud.hs-mannheim.de>
2024-06-19 22:07:39 +0200

    roulette_logic_test erstellt

---
+++ test/roulette_logic_test.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hsma_cpd_project/logic/roulette_logic.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();  // Initialisiere die Flutter-Bindung
+  group('RouletteLogic', () {
+    test('startRoulette sets up the animation correctly', () {
+      final rouletteLogic = RouletteLogic(TestVSync());
+      final randomNumber = 5;
+
+      rouletteLogic.startRoulette(randomNumber);
+
+      expect(rouletteLogic.animation.status, AnimationStatus.forward);
+      expect(rouletteLogic.initialVelocity, isNotNull);
+      expect(rouletteLogic.acceleration, isNotNull);
+    });
+
+    test('calculateCurrentAngle calculates the angle correctly', () {
+      final rouletteLogic = RouletteLogic(TestVSync());
+      final randomNumber = 5;
+
+      rouletteLogic.startRoulette(randomNumber);
+
+      final angle = rouletteLogic.calculateCurrentAngle();
+
+      expect(angle, isNotNull);
+      expect(angle, isNonNegative);
+    });
+  });
+}
